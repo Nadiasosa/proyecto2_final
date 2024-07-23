00695994c3ceea095ee5bc9c9a9228ab6b2e867a
fede2324 <feder6772@gmail.com>
2024-07-23 19:14:42 -0300

    correcion contacto

--- js/validator.js
+++ js/validator.js
@@ -70,7 +70,7 @@ function validateEmail(email) {
         formData.append('message', message);
 
         // Enviar los datos a formsubmit.co
-        fetch('https://formsubmit.co/34a6dfed0957f008921dc6b0959f8f8d  ', {
+        fetch('https://formsubmit.co/federg772@gmail.com', {
         method: 'POST',
         headers: {
             'Accept': 'application/json'
